1b3b3869cf0b3edd55b5d3ab397b336d19dde4ac
Tiago Mergulhão <me@tmergulhao.com>
2018-11-26 19:23:27 -0500

    Links to content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, navigate, withPrefix } from 'gatsby'
+import { withPrefix } from 'gatsby'
 import styled from 'styled-components'
 
 import Header from '../components/header'
@@ -14,7 +14,7 @@ const TwoColumnWrapper = styled.div`
   }
 `
 
-const ProjectWrapper = styled.div`
+const ProjectWrapper = styled.a`
   position: relative;
   & > * {
     margin-left: 80px;
@@ -29,69 +29,20 @@ const ProjectWrapper = styled.div`
   }
 `
 
-const Project = ({ title, path, body, img }) =>
-  <ProjectWrapper onClick={ () => navigate('/' + path) } role="link" tabIndex={0}>
+const Project = ({ title, link, body, img }) =>
+  <ProjectWrapper href={ link }>
     <img src={ img }/>
     <h3 className='accent'>{ title }</h3>
     <p>{ body }</p>
   </ProjectWrapper>
 
-const OpenSource = () =>
-  <>
-    <h2>Open Source</h2>
-    <TwoColumnWrapper>
-    <Project
-      img={ withPrefix('/viewanimator.svg') }
-      path=''
-      title='View Animator'
-      body='Animate an entire view hierarchy with just one line of code.'/>
-    <Project
-      img={ withPrefix('/sizes.svg') }
-      path=''
-      title='Sizes'
-      body='Drastically reduce effort on testing your app on different sizes'/>
-    <Project
-      img={ withPrefix('/easytransition.svg') }
-      path=''
-      title='Easy Transitions'
-      body='Most common interactions that drive transitions on one-line'/>
-    <Project
-      img={ withPrefix('/swipetabbar.svg') }
-      path=''
-      title='Swipeable Tab Bar Controller'
-      body="Tab Bar that let's you swipe interactively with different transitions"/>
-    </TwoColumnWrapper>
-  </>
-
-const Talks = () =>
-  <div>
-    <h2>Talks</h2>
-    <ul>
-      <li><a>Exploring interactive transitions, 2018 (Spanish)</a></li>
-      <li><a>Evolving with Swift, 2018 (Spanish)</a></li>
-    </ul>
-  </div>
-
-const Podcasts = () =>
-  <div>
-    <h2>Podcasts</h2>
-    <ul>
-      <li><a>Swift Coders, episode 59</a></li>
-      <li><a>iPhreaks, episode 224</a></li>
-    </ul>
-    <p>I’ve been invited as a guest to a few different podscasts and I loved each and everyone of them. I’m open to this type of collaboration so hit me up if you have an episode idea which I can participate in.</p>
-  </div>
-
-const HireMe = () =>
-  <>
-    <h2>Hire Me</h2>
-    <p>I’m always interested in new projects, consulting or doing code review. I mostly work via <a href='https://www.toptal.com/#expect-just-trusted-coders'>Toptal</a>.</p>
-  </>
-
 const IconizedList = styled.ul`
   & > li > img {
     margin-bottom: -2px;
     margin-right: 8px;
+  }
+  li > a {
+    color: inherit;
   }
 `
 
@@ -100,7 +51,7 @@ const IndexPage = () =>
   <Header/>
   <Layout>
     <IconizedList>
-      <li><img src='/Twitter.svg'/>@marcosgriselli</li>
+      <li><a href='https://twitter.com/marcosgriselli'><img src='/Twitter.svg'/>@marcosgriselli</a></li>
       <li><img src='/Swift.svg'/>iOS Developer</li>
       <li><img src='/Location.svg'/>Buenos Aires, Argentina (GMT-3)</li>
     </IconizedList>
@@ -108,12 +59,52 @@ const IndexPage = () =>
     <TwoColumnWrapper>
       <Blogs/>
     </TwoColumnWrapper>
-    <OpenSource/>
+    <>
+      <h2>Open Source</h2>
+      <TwoColumnWrapper>
+      <Project
+        img={ withPrefix('/viewanimator.svg') }
+        link='https://github.com/marcosgriselli/ViewAnimator'
+        title='View Animator'
+        body='Animate an entire view hierarchy with just one line of code.'/>
+      <Project
+        img={ withPrefix('/sizes.svg') }
+        link='https://github.com/marcosgriselli/sizes'
+        title='Sizes'
+        body='Drastically reduce effort on testing your app on different sizes'/>
+      <Project
+        img={ withPrefix('/easytransition.svg') }
+        link='https://github.com/marcosgriselli/easytransitions'
+        title='Easy Transitions'
+        body='Most common interactions that drive transitions on one-line'/>
+      <Project
+        img={ withPrefix('/swipetabbar.svg') }
+        link='https://github.com/marcosgriselli/SwipeableTabBarController'
+        title='Swipeable Tab Bar Controller'
+        body="Tab Bar that let's you swipe interactively with different transitions"/>
+      </TwoColumnWrapper>
+    </>
     <TwoColumnWrapper>
-      <Talks/>
-      <Podcasts/>
+      <div>
+        <h2>Talks</h2>
+        <ul>
+          <li><a href='https://vimeo.com/268789126'>Exploring interactive transitions, 2018 (Spanish)</a></li>
+          <li><a href='https://speakerdeck.com/marcosgriselli/evolucionando-con-swift'>Evolving with Swift, 2018 (Spanish)</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2>Podcasts</h2>
+        <ul>
+          <li><a href='https://www.podbean.com/media/share/pb-ffd78-6c604b'>Swift Coders, episode 59</a></li>
+          <li><a href='https://devchat.tv/iphreaks/ips-224-design-code-meng-marcos-griselli'>iPhreaks, episode 224</a></li>
+        </ul>
+        <p>I’ve been invited as a guest to a few different podscasts and I loved each and everyone of them. I’m open to this type of collaboration so hit me up if you have an episode idea which I can participate in.</p>
+      </div>
     </TwoColumnWrapper>
-    <HireMe/>
+    <>
+      <h2>Hire Me</h2>
+      <p>I’m always interested in new projects, consulting or doing code review. I mostly work via <a href='https://www.toptal.com/#expect-just-trusted-coders'>Toptal</a>.</p>
+    </>
   </Layout>
   </>
 
