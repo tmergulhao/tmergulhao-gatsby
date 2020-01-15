f207e724e3a950821924d68ddf0611e3fd671968
Tiago Mergulhão <me@tmergulhao.com>
2020-01-15 11:02:09 -0300

    Removing more stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
-    'gatsby-plugin-typescript',
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
     {
