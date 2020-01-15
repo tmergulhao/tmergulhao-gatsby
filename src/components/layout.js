bad0db8d097f8c0500e9136e84c596194790ad54
Tiago Mergulhão <me@tmergulhao.com>
2020-01-15 11:03:52 -0300

    Simplification

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import "./layout.scss"
 import "./overrides.scss"
+import "./index.scss"
 
 const Layout = ({ children }) =>
   <main className="layout">{children}</main>
